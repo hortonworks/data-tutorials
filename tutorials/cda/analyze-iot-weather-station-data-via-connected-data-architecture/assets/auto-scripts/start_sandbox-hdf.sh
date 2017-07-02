0524f2eb30b2974b8e19e0ef970b7b8c6bf69ff5
James Medel <jamesmedel94@gmail.com>
2017-07-02 14:49:01 -0700

    removed auto_scripts folder
    all auto scripts have been removed. Planning to create a future auto
    script just for testing the NiFi tutorial series

--- tutorials/cda/analyze-iot-weather-station-data-via-connected-data-architecture/assets/auto-scripts/start_sandbox-hdf.sh
+++ tutorials/cda/analyze-iot-weather-station-data-via-connected-data-architecture/assets/auto-scripts/start_sandbox-hdf.sh
@@ -8,50 +8,53 @@ else
 docker run --name sandbox-hdf --hostname "sandbox-hdf.hortonworks.com" --privileged -d \
 -p 9999:9999 \
 -p 12181:2181 \
--p 17010:17010 \
--p 51070:51070 \
--p 13000:13000 \
+-p 26000:16000 \
+-p 17010:16010 \
+-p 17020:16020 \
+-p 17030:16030 \
+-p 51070:50070 \
+-p 13000:3000 \
 -p 14200:4200 \
 -p 14557:4557 \
 -p 16080:6080 \
 -p 18000:8000 \
 -p 9080:8080 \
--p 18088:18088 \
--p 18744:18744 \
--p 18886:18886 \
--p 28886:28886 \
+-p 18088:8088 \
+-p 18744:8744 \
+-p 28886:8886 \
+-p 38886:18886 \
 -p 18888:8888 \
 -p 18993:8993 \
 -p 19000:9000 \
--p 19090:19090 \
--p 19091:9091 \
--p 42111:42111 \
--p 62888:62888 \
--p 25100:15100 \
--p 25101:15101 \
--p 25102:15102 \
--p 25103:15103 \
--p 25104:15104 \
--p 25105:15105 \
--p 27000:17000 \
--p 27001:17001 \
--p 27002:17002 \
--p 27003:17003 \
--p 27004:17004 \
--p 27005:17005 \
--p 18081:18081 \
--p 18090:18090 \
--p 19060:19060 \
--p 19089:19089 \
--p 19888:19888 \
+-p 9090:9090 \
+-p 9091:9091 \
+-p 43111:42111 \
+-p 62888:61888 \
+-p 15100:15100 \
+-p 15101:15101 \
+-p 15102:15102 \
+-p 15103:15103 \
+-p 15104:15104 \
+-p 15105:15105 \
+-p 17000:17000 \
+-p 17001:17001 \
+-p 17002:17002 \
+-p 17003:17003 \
+-p 17004:17004 \
+-p 17005:17005 \
+-p 18081:8081 \
+-p 18090:8090 \
+-p 19060:9060 \
+-p 19089:9089 \
+-p 29888:29888 \
 -p 16667:6667 \
--p 17777:17777 \
--p 17788:17788 \
+-p 17777:7777 \
+-p 17778:7778 \
+-p 17788:7788 \
 -p 17789:7789 \
 -p 12222:22 \
 sandbox-hdf /usr/sbin/sshd -D
 fi
-echo "Starting Ambari Services, may take 2.5 minutes..."
 docker exec -d sandbox-hdf service mysqld start
 docker exec -d sandbox-hdf service postgresql start
 docker exec -t sandbox-hdf ambari-server start
@@ -61,4 +64,3 @@ docker exec -d sandbox-hdf /root/start_sandbox.sh
 docker exec -d sandbox-hdf /etc/init.d/tutorials start
 docker exec -d sandbox-hdf /etc/init.d/shellinaboxd start
 echo "Successfully Started HDF Sandbox Container"
-sleep 150
