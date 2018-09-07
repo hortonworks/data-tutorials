f6f31196661386b1381c2be7facb2fe2581a7776
James Medel <jamesmedel94@gmail.com>
2018-09-07 16:19:38 -0700

    Created setup-hdp.sh script
    Sets up HDP sandbox  development environment by configuring services
    used in the security breach application development. Turns off Spark’s
    maintenance mode.

---
+++ tutorials/cda/building-a-security-breach-analysis-application/application/setup/shell/setup-hdp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##
+# Run script in HDP Sandbox CentOS7
+# Sets up HDP Dev Environment, so User can focus on Spark Data Analysis
+# 1. Turns off Spark maintenance mode via Ambari
+##
+
+# Setup Spark Service on HDP Sandbox:
+# Turn off Spark's maintenance mode via Ambari
+
+HDP_AMBARI_USER="raj_ops"
+HDP_AMBARI_PASS="raj_ops"
+HDP_CLUSTER_NAME="Sandbox"
+HDP_HOST="sandbox-hdp.hortonworks.com"
+HDP="hdp-sandbox"
+
+echo "Setting up HDP Sandbox Environment for Spark Data Analysis..."
+echo "Turning off Spark's maintenance mode via Ambari"
+AMBARI_CREDENTIALS=$HDP_AMBARI_USER:$HDP_AMBARI_PASS
+curl -k -u $AMBARI_CREDENTIALS -H "X-Requested-By:ambari" -i -X PUT -d \
+'{"RequestInfo": {"context":"Turn off Maintenance for SPARK"}, "Body":
+{"ServiceInfo": {"maintenance_state":"OFF"}}}' \
+http://$HDP_HOST:8080/api/v1/clusters/$HDP_CLUSTER_NAME/services/SPARK
