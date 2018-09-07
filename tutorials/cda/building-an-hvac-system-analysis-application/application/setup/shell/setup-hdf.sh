08780fb97dd585c1677e4b9d2510f4f0263319a9
James Medel <jamesmedel94@gmail.com>
2018-09-06 21:44:17 -0700

    Created setup-hdf.sh shell script
    This setup-hdf.sh shell script was created to show users how to
    automate setup of HDF platform, so they can get to actually building
    the HVAC Systems Analysis demo.

---
+++ tutorials/cda/building-an-hvac-system-analysis-application/application/setup/shell/setup-hdf.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+##
+# Script sets up HDF services used in Building an HVAC System Analysis Application
+##
+
+##
+# Purpose: NiFi GetHTTP will run into ERROR cause it can't resolve
+# S3 Domain Name Server (DNS)
+#
+# Potential Solution: Append Google Public DNS to CentOS7 /etc/resolve.conf
+# CentOS7 is the OS of the server NiFi runs on. Google Public DNS is able to
+# resolve S3 DNS. Thus, when NiFi flow runs, GetHTTP is able to do the download
+# of HVAC Sensor Data.
+##
+
+# Adding Public DNS to resolve msg: unable to resolv s3.amazonaws.com
+# https://forums.aws.amazon.com/thread.jspa?threadID=125056
+tee -a /etc/resolve.conf << EOF
+# Google Public DNS
+nameserver 8.8.8.8
+EOF
+
+##
+# Purpose: HDF Sandbox comes with a prebuilt NiFi flow, which causes user to be
+# pulled away from building the HVAC System Analysis Application.
+#
+# Potential Solution: Backup prebuilt NiFi flow and call it a different name.
+##
+
+HDF_AMBARI_USER="admin"
+HDF_AMBARI_PASS="admin"
+HDF_CLUSTER_NAME="Sandbox"
+HDF_HOST="sandbox-hdf.hortonworks.com"
+HDF="hdf-sandbox"
+AMBARI_CREDENTIALS=$HDF_AMBARI_USER:$HDF_AMBARI_PASS
+
+# Ambari REST Call Function: waits on service action completing
+
+# Start Service in Ambari Stack and wait for it
+# $1: HDF or HDP
+# $2: Service
+# $3: Status - STARTED or INSTALLED, but OFF
+function wait()
+{
+  if [[ $1 == "hdp-sandbox" ]]
+  then
+    finished=0
+    while [ $finished -ne 1 ]
+    do
+      ENDPOINT="http://$HDP_HOST:8080/api/v1/clusters/$HDP_CLUSTER_NAME/services/$2"
+      AMBARI_CREDENTIALS="$HDP_AMBARI_USER:$HDP_AMBARI_PASS"
+      str=$(curl -s -u $AMBARI_CREDENTIALS $ENDPOINT)
+      if [[ $str == *"$3"* ]] || [[ $str == *"Service not found"* ]]
+      then
+        finished=1
+      fi
+        sleep 3
+    done
+  elif [[ $1 == "hdf-sandbox" ]]
+  then
+    finished=0
+    while [ $finished -ne 1 ]
+    do
+      ENDPOINT="http://$HDF_HOST:8080/api/v1/clusters/$HDF_CLUSTER_NAME/services/$2"
+      AMBARI_CREDENTIALS="$HDF_AMBARI_USER:$HDF_AMBARI_PASS"
+      str=$(curl -s -u $AMBARI_CREDENTIALS $ENDPOINT)
+      if [[ $str == *"$3"* ]] || [[ $str == *"Service not found"* ]]
+      then
+        finished=1
+      fi
+        sleep 3
+    done
+  else
+    echo "ERROR: Didn't Wait for Service, need to choose appropriate sandbox HDF or HDP"
+  fi
+}
+
+# Stop NiFi first, then backup prebuilt NiFi flow, then start NiFi for
+# changes to take effect
+
+echo "Stopping NiFi via Ambari"
+curl -u $AMBARI_CREDENTIALS -H "X-Requested-By: ambari" -X PUT -d '{"RequestInfo":
+{"context": "Stop NiFi"}, "ServiceInfo": {"state": "INSTALLED"}}' \
+http://$HDF_HOST:8080/api/v1/clusters/$HDF_CLUSTER_NAME/services/NIFI
+wait $HDF NIFI "INSTALLED"
+
+echo "Prebuilt flow on NiFi canvas backed up to flow.xml.gz.bak"
+mv /var/lib/nifi/conf/flow.xml.gz /var/lib/nifi/conf/flow.xml.gz.bak
+
+echo "Starting NiFi via Ambari"
+curl -u $AMBARI_CREDENTIALS -H "X-Requested-By: ambari" -X PUT -d '{"RequestInfo":
+{"context": "Start NiFi"}, "ServiceInfo": {"state": "STARTED"}}' \
+http://$HDF_HOST:8080/api/v1/clusters/$HDF_CLUSTER_NAME/services/NIFI
+wait $HDF NIFI "STARTED"
