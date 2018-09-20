0d960c907e7049233199e609dee9f7534d8fa702
James <jmedel@hortonworks.com>
2018-09-20 07:47:24 -0700

    update with hdfs created hvac_building and hvac_machine and allow anyone rwe

--- tutorials/cda/building-an-hvac-system-analysis-application/application/setup/shell/setup-hdp.sh
+++ tutorials/cda/building-an-hvac-system-analysis-application/application/setup/shell/setup-hdp.sh
@@ -10,6 +10,6 @@
 # allow read-write-execute permissions for the owner, group, and any other users
 
 su hdfs
-hdfs dfs -mkdir -p /sandbox/
-hdfs dfs -chmod -R 777 /sandbox/
+hdfs dfs -mkdir -p /sandbox/sensor/hvac_building/ /sandbox/sensor/hvac_machine
+hdfs dfs -chmod -R 777 /sandbox/sensor/hvac_building/ /sandbox/sensor/hvac_machine
 exit
