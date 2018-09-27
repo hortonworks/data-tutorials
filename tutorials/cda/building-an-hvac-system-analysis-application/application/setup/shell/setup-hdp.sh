84d53c03c2cbad74406d6394101c1f17b7184a60
James <jmedel@hortonworks.com>
2018-09-27 14:51:24 -0700

    Update added DATE of echo log to script

--- tutorials/cda/building-an-hvac-system-analysis-application/application/setup/shell/setup-hdp.sh
+++ tutorials/cda/building-an-hvac-system-analysis-application/application/setup/shell/setup-hdp.sh
@@ -10,6 +10,7 @@
 # Author: James Medel
 # Email: jamesmedel94@gmail.com
 ##
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo "Setting Up HDP Dev Environment for HVAC System Analysis App"
 
 # Creates /sandbox directory in HDFS
@@ -17,14 +18,15 @@ echo "Setting Up HDP Dev Environment for HVAC System Analysis App"
 mkdir -p /var/log/cda-sb/310/
 setup_hdfs()
 {
-  echo "INFO: Creating /sandbox/sensor/hvac_building and /sandbox/sensor/hvac_machine"
+  echo "$DATE INFO: Creating /sandbox/sensor/hvac_building and /sandbox/sensor/hvac_machine"
   sudo -u hdfs hdfs dfs -mkdir -p /sandbox/sensor/hvac_building/
   sudo -u hdfs hdfs dfs -mkdir /sandbox/sensor/hvac_machine
-  echo "INFO: Setting permissions for hvac_buildnig and hvac_machine to 777"
+  echo "$DATE INFO: Setting permissions for hvac_buildnig and hvac_machine to 777"
   sudo -u hdfs hdfs dfs -chmod -R 777 /sandbox/sensor/hvac_building/
   sudo -u hdfs hdfs dfs -chmod -R 777 /sandbox/sensor/hvac_machine
-  echo "INFO: Checking both directories were created and permissions were set"
+  echo "$DATE INFO: Checking both directories were created and permissions were set"
   sudo -u hdfs hdfs dfs -ls /sandbox/sensor
 }
 # Log everything, but also output to stdout
+echo "$DATE INFO: Executing setup_hdfs bash function, logging to /var/log/cda-sb/310/setup-hdp.log"
 setup_hdfs | tee -a /var/log/cda-sb/310/setup-hdp.log
