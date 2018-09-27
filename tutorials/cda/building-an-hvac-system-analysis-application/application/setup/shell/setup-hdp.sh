cc007b7c324fa6433f82517de240cf3dff260c7b
James <jmedel@hortonworks.com>
2018-09-27 13:53:58 -0700

    updated script to log everything

--- tutorials/cda/building-an-hvac-system-analysis-application/application/setup/shell/setup-hdp.sh
+++ tutorials/cda/building-an-hvac-system-analysis-application/application/setup/shell/setup-hdp.sh
@@ -14,13 +14,17 @@ echo "Setting Up HDP Dev Environment for HVAC System Analysis App"
 
 # Creates /sandbox directory in HDFS
 # allow read-write-execute permissions for the owner, group, and any other users
-
-su hdfs
-echo "Creating /sandbox/sensor/hvac_building and /sandbox/sensor/hvac_machine"
-hdfs dfs -mkdir -p /sandbox/sensor/hvac_building/
-hdfs dfs -mkdir /sandbox/sensor/hvac_machine
-hdfs dfs -chmod -R 777 /sandbox/sensor/hvac_building/
-hdfs dfs -chmod -R 777 /sandbox/sensor/hvac_machine
-echo "Checking both directories were created and permissions were set"
-hdfs dfs -ls /sandbox/sensor
-exit
+setup_hdfs()
+{
+  su hdfs
+  echo "INFO: Creating /sandbox/sensor/hvac_building and /sandbox/sensor/hvac_machine"
+  hdfs dfs -mkdir -p /sandbox/sensor/hvac_building/
+  hdfs dfs -mkdir /sandbox/sensor/hvac_machine
+  hdfs dfs -chmod -R 777 /sandbox/sensor/hvac_building/
+  hdfs dfs -chmod -R 777 /sandbox/sensor/hvac_machine
+  echo "INFO: Checking both directories were created and permissions were set"
+  hdfs dfs -ls /sandbox/sensor
+  exit
+}
+# Log everything, but also output to stdout
+setup_hdfs 2>&1 | tee -a /var/log/cda-sb/310/setup-hdp.log
