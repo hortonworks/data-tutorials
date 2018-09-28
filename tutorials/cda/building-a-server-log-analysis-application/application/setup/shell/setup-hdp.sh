5b7155a3091cdf4ab97f61178f292448219ae646
James <jmedel@hortonworks.com>
2018-09-27 17:59:58 -0700

    Added logging to setting up hdp shell script

--- tutorials/cda/building-a-server-log-analysis-application/application/setup/shell/setup-hdp.sh
+++ tutorials/cda/building-a-server-log-analysis-application/application/setup/shell/setup-hdp.sh
@@ -6,45 +6,46 @@
 ##
 
 ##
-# Run script in HDP Sandbox CentOS7
 # Sets up HDP Dev Environment, so User can focus on Spark Data Analysis
 # 1. Turns off Spark maintenance mode via Ambari
 ##
 
-# Setup Spark Service on HDP Sandbox:
-# Turn off Spark's maintenance mode via Ambari
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+LOG_DIR_BASE="/var/log/cda-sb/200/"
+echo "Setting Up HDP Dev Environment for Server Log Analysis App"
+mkdir -p $LOG_DIR_BASE/hdp
 
-HDP="hdp-sandbox"
-HDP_AMBARI_USER="$1" # $1: Expects user to pass "Ambari User" into the file
-HDP_AMBARI_PASS="$2" # $2: Expects user to pass "Ambari Admin Password" into the file
-HDP_CLUSTER_NAME="Sandbox"
-HDP_HOST="sandbox-hdp.hortonworks.com"
-AMBARI_CREDENTIALS=$HDP_AMBARI_USER:$HDP_AMBARI_PASS
+setup_public_dns()
+{
+  echo "$DATE INFO: Adding Google Public DNS to /etc/resolve.conf"
+  echo "# Google Public DNS" | tee -a /etc/resolve.conf
+  echo "nameserver 8.8.8.8" | tee -a /etc/resolve.conf
 
-echo "Setting up HDP Sandbox Development Environment"
-tee -a /etc/resolv.conf << EOF
-# Google's Public DNS
-nameserver 8.8.8.8
-EOF
+  echo "$DATE INFO: Checking Google Public DNS added to /etc/resolve.conf"
+  cat /etc/resolve.conf
 
-echo "Create Directory for Zeppelin Notebooks"
-mkdir -p /sandbox/tutorial-files/200/zeppelin/notebooks/
-chmod -R 777 /sandbox/tutorial-files/200/zeppelin/notebooks/
+  # Log everything, but also output to stdout
+  echo "$DATE INFO: Executing setup_nifi bash function, logging to $LOG_DIR_BASE/hdf/setup-public-dns.log"
+}
 
-echo "Create /sandbox/tutorial-files/200/nifi/"
-echo "Allow read-write-execute permissions to any user, temp solution for nifi"
-# Creates /sandbox directory in HDFS
-# allow read-write-execute permissions for the owner, group, and any other users
+setup_zeppelin()
+{
+  echo "$DATE INFO: Creating Directory for Zeppelin Notebooks"
+  mkdir -p /sandbox/tutorial-files/200/zeppelin/notebooks/
+  echo "$DATE INFO: Allowing read-write-execute permissions to any user, for zeppelin REST Call"
+  chmod -R 777 /sandbox/tutorial-files/200/zeppelin/notebooks/
+}
 
-su hdfs
-hdfs dfs -mkdir -p /sandbox/tutorial-files/200/nifi/
-hdfs dfs -chmod -R 777 /sandbox/tutorial-files/200/nifi/
-exit
+setup_hdfs()
+{
+  # Creates /sandbox directory in HDFS
+  # allow read-write-execute permissions for the owner, group, and any other users
+  echo "$DATE INFO: Creating HDFS dir /sandbox/tutorial-files/200/nifi/ for HDF NiFi to write data"
+  hdfs dfs -mkdir -p /sandbox/tutorial-files/200/nifi/
+  echo "$DATE INFO: Allowing read-write-execute permissions to any user, so NiFi has write access"
+  hdfs dfs -chmod -R 777 /sandbox/tutorial-files/200/nifi/
+}
 
-# TODO: Check that service exists via ambari rest call
-# TODO: Check if service is in maintenance mode via ambari rest call
-echo "Turning off Spark's maintenance mode via Ambari"
-curl -k -u $AMBARI_CREDENTIALS -H "X-Requested-By:ambari" -i -X PUT -d \
-'{"RequestInfo": {"context":"Turn off Maintenance for SPARK"}, "Body":
-{"ServiceInfo": {"maintenance_state":"OFF"}}}' \
-http://$HDP_HOST:8080/api/v1/clusters/$HDP_CLUSTER_NAME/services/SPARK2
+setup_public_dns | tee -a $LOG_DIR_BASE/hdp/setup-public-dns.log
+setup_zeppelin | tee -a $LOG_DIR_BASE/hdp/setup-zeppelin.log
+setup_hdfs | tee -a $LOG_DIR_BASE/hdp/setup-hdfs.log
